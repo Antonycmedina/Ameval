11d3e1ba2de9c1ad072e832df5f0f7d5c4e3f2ef
Antony <antonycmedina@gmail.com>
2018-01-26 23:03:13 +0100

    Actualización de la Galería
    Actualización de la galería, y primeros todos a otras secciones de la
    web.
    Sólo tener en cuenta la VERSIÓN MÓVIL.

--- js/main.js
+++ js/main.js
@@ -3,26 +3,25 @@ var num = 1;
 var avanzar = function() {
 	num++;
 	if (num<4) {
-		var img = document.getElementById("imagen").src = "img/"+num+".jpg";
+		var img = document.getElementById("home").style.backgroundImage = "url(img/"+num+".jpg)";
 	} else if(num==4) {
 		num=1;
-		var img = document.getElementById("imagen").src = "img/"+num+".jpg";
+		var img = document.getElementById("home").style.backgroundImage = "url(img/"+num+".jpg)";
 	}
 };
 
 var retroceder = function() {
 	num--;
 	if (num>0) {
-		var img = document.getElementById("imagen").src = "img/"+num+".jpg";
+		var img = document.getElementById("home").style.backgroundImage = "url(img/"+num+".jpg)";
 	} else if (num==0) {
 		num=3;
-		var img = document.getElementById("imagen").src = "img/"+num+".jpg";
+		var img = document.getElementById("home").style.backgroundImage = "url(img/"+num+".jpg)";
 	}
 };
 
 
 window.onload = function () {
-	//alert(window.outerWidth +"\n"+ window.outerHeight);
   //header
   var contador = 1;
   document.getElementById("boton-span").addEventListener("click", function () {
@@ -40,4 +39,11 @@ window.onload = function () {
   document.getElementById("avanzar").onclick = avanzar;
   document.getElementById("retroceder").onclick = retroceder;
 	setInterval(avanzar,4000);
+
+	//productos
+	var productos = document.getElementsByTagName("figure");
+	for (var i = 0; i <= productos.length; i++) {
+		console.log(i)
+	}
+	console.log(productos);
 };
